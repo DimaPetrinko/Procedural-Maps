360b8d10160fc7b37be50eea6d642bbe404b8f69
DimaPetrinko <petrinko6@gmail.com>
2016-09-10 00:37:05 +0300

    Initial 2.0
    imported existing project

---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public static class Noise
+{
+    public static float Value(Vector3 point, float frequency)
+    {
+        point *= frequency;
+        int v = (int)point.x;
+        return v % 2;
+    }
+
+}
