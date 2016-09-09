9528f4484fdc1acf49c6138070eea1f7ca57b711
Blind56 <petrinko6@gmail.com>
2016-09-10 02:36:34 +0300

    Perlin Done

--- Assets/Scripts/TextureCreator.cs
+++ Assets/Scripts/TextureCreator.cs
@@ -5,6 +5,9 @@ public class TextureCreator : MonoBehaviour
 {
     [Range(2, 512)]
     public int resolution = 256;
+    [Range(1, 3)]
+    public int dimensions = 3;
+    public NoiseMethodType type;
     public float frequency = 1f;
 
     private Texture2D texture;
@@ -13,10 +16,10 @@ public class TextureCreator : MonoBehaviour
     {
         if (texture == null)
         {
-            texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, true);
+            texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, false);
             texture.name = "Procedural Texture";
             texture.wrapMode = TextureWrapMode.Clamp;
-            texture.filterMode = FilterMode.Trilinear;
+            texture.filterMode = FilterMode.Point;
             texture.anisoLevel = 9;
             GetComponent<MeshRenderer>().material.mainTexture = texture;
         }
@@ -44,6 +47,7 @@ public class TextureCreator : MonoBehaviour
         Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
         Vector3 point11 = transform.TransformPoint(new Vector3(0.5f, 0.5f));
 
+        NoiseMethod method = Noise.noiseMethods[(int)type][dimensions - 1];
         float step = 1f / resolution;
 		
         for (int y = 0; y < resolution; y++)
@@ -53,7 +57,12 @@ public class TextureCreator : MonoBehaviour
             for (int x = 0; x < resolution; x++)
             {
                 Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * step);
-                texture.SetPixel(x, y, Color.white * Noise.Value(point, frequency));
+                float sample = method(point, frequency);
+                if (type != NoiseMethodType.Value)
+                {
+                    sample = sample * 0.5f + 0.5f;
+                }
+                texture.SetPixel(x, y, Color.white * sample);
             }
         }
         texture.Apply();
